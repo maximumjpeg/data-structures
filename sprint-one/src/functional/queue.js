6386335b39293929af48a3d0851caf7dfe5f1648
Jordan Addleman <xibalbarising@gmail.com>
2022-04-14 12:06:35 -0400

    passed all tests in functional/queue.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -6,14 +6,35 @@ var Queue = function() {
 
   // Implement the methods below
 
+  // closure for right and left pointers.
+  var left = undefined;
+  var right = undefined;
+
   someInstance.enqueue = function(value) {
+
+
+    if (Object.keys(storage).length === 0) {
+      left = 0;
+      right = 0;
+      storage[0] = value;
+    } else {
+      right++;
+      storage[right] = value;
+    }
   };
 
   someInstance.dequeue = function() {
+    var dequeued = storage[left];
+    delete storage[left];
+    left++;
+    return dequeued;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
 };
+
+
