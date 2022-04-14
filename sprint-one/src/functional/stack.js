a63e27cc2fe19246f758c1458698b8c1f2c825bb
Jordan Addleman <xibalbarising@gmail.com>
2022-04-13 22:43:32 -0400

    passed all tests in functional>stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,24 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    if (Object.keys(storage).length === 0) {
+      console.log('empty storage');
+      storage[0] = value;
+    } else {
+      storage[Object.keys(storage).length] = value;
+      console.log('storage was not empty');
+    }
+
   };
 
   someInstance.pop = function() {
+    var popped = storage[Object.keys(storage).length - 1];
+    delete storage[Object.keys(storage).length - 1];
+    return popped;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
