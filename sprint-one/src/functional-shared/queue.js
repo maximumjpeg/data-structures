38c90312fb730b3e378790f35651a2e38c73bc1a
Jordan Addleman <xibalbarising@gmail.com>
2022-04-14 15:26:56 -0400

    passing all but 2 tests in functional-shared>queue.js

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,41 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+
+  instance.storage = {};
+
+  instance.population = 0;
+
+  instance.left = 0;
+  instance.right = 0;
+
+  _.extend(instance, queueMethods);
+
+  return instance;
+
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.population] = value;
+  this.population++;
+};
+
+queueMethods.dequeue = function() {
+  var popped = this.storage[this.left];
+  delete this.storage[this.left];
+  this.left++;
+  this.population--;
+  return popped;
+};
+
+queueMethods.size = function() {
+  if (this.population < 0) {
+    return 0;
+  } else {
+    return this.population;
+  }
+};
 
