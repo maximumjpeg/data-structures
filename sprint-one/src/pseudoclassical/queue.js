549a067a10450033f8ecc22f57cfbec06d214455
Jordan Addleman <xibalbarising@gmail.com>
2022-04-14 23:01:40 -0400

    passing all tests in pseudoclassical>que.js and slight refactor pseudo>stack.js

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,39 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.population = 0;
+  this.left = 0;
+  this.right = 0;
 };
+
+Queue.prototype.enqueue = function(value) {
+  if (this.population === 0) {
+    this.storage[this.left] = value;
+    this.population++;
+  } else {
+    this.storage[this.right + 1] = value;
+    this.population++;
+    this.right++;
+  }
+};
+
+Queue.prototype.dequeue = function() {
+  var dequeued = this.storage[this.left];
+  delete this.storage[this.left];
+  this.left++;
+  this.population--;
+  return dequeued;
+};
+
+Queue.prototype.size = function() {
+  if (this.population < 0) {
+    return 0;
+  } else {
+    return this.population;
+  }
+};
+
+var instanced = new Queue();
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -9,12 +9,14 @@ Stack.prototype.push = function(value) {
   this.storage[this.population] = value;
   this.population++;
 };
+
 Stack.prototype.pop = function() {
   var popped = this.storage[this.population - 1];
   delete this.storage[this.population - 1];
   this.population--;
   return popped;
 };
+
 Stack.prototype.size = function() {
   if (this.population < 0) {
     return 0;
