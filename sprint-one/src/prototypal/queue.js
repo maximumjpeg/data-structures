62faf472b99aefff21020de3eb3d170758a34cc9
Jordan Addleman <xibalbarising@gmail.com>
2022-04-14 21:48:02 -0400

    passed all tests in prototypal>queue.js

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,39 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newInstance = Object.create(queueMethods);
+  return newInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  storage: {},
+  population: 0,
+  left: 0,
+  right: 0,
+  enqueue: function(value) {
+    if (this.population === 0) {
+      this.storage[this.left] = value;
+      this.population++;
+    } else {
+      this.storage[this.right + 1] = value;
+      this.population++;
+      this.right++;
+    }
+  },
+  dequeue: function() {
+    var dequeued = this.storage[this.left];
+    delete this.storage[this.left];
+    this.left++;
+    this.population--;
+    return dequeued;
+  },
+  size: function() {
+    if (this.population < 0) {
+      return 0;
+    } else {
+      return this.population;
+    }
+  }
+};
 
 
