5f15e0b95a3a6361bcdb9e76975350a01c296a18
Jordan Addleman <xibalbarising@gmail.com>
2022-04-15 23:12:12 -0400

    passing all but two tests in tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,11 +13,37 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  // does called on tree contain target
+  if (this.value === target) {
+    // if so, return true
+    return true;
+  }
+  // target not in called on tree
+  // does called on tree have children?
+  // if so...
+  if (this.children.length > 0) {
+    // iterate through this.children
+    for (var i = 0; i < this.children.length; i++) {
+      // if item (tree) has children
+      if (this.children[i].children.length > 0) {
+        // recurse this fn call
+        this.children[i].contains(target);
+        // item (tree) has no children
+        if (this.children[i].length === 0) {
+          // check current tree for target
+          if (this.children[i].value === target) {
+            return true;
+          }
+        }
+      }
+    }
+  }
+  return false;
 };
-
 
 
 /*
